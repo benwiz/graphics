2a9e47ec48177626226a53400cff3befd208e21a
Ben Wisialowski <bwisialowski@gmail.com>
2018-03-11 09:52:44 -0500

    better log

--- py-detect-features/deploy.sh
+++ py-detect-features/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+rm py-detect-features.zip
 mv linux-libs/cv2 .
 mv linux-libs/numpy .
-zip -r9 py-detect-features.zip * -x *.zip -x deploy.sh -x *linux-libs/*
+zip -r9 py-detect-features.zip * -x *.zip -x deploy.sh
 mv cv2 linux-libs/
 mv numpy linux-libs/
 # aws lambda update-function-code --function-name lowpoly-py-detect-features --zip-file fileb://$(pwd)/py-detect-features.zip --profile personal
-aws s3 cp py-detect-features.zip s3://lowpoly/py-detect-features.py --profile personal
+aws s3 cp py-detect-features.zip s3://lowpoly/py-detect-features.zip --profile personal
--- py-detect-features/lambda_function.py
+++ py-detect-features/lambda_function.py
@@ -1,9 +1,10 @@
 import cv2
+import json
 
 
 def lambda_handler(event, context):
     print "OpenCV installed version:", cv2.__version__
-    print "Event:\n", event
+    print "Event:\n", json.dumps(event)
     print "Context:\n", context
     return "It works!"
 
