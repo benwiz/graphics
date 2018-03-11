0e238d76cf574154e6876c48cbb50172770e880b
Ben Wisialowski <bwisialowski@gmail.com>
2018-03-10 21:50:17 -0600

    run delaunay triangulation

--- partition/src/partition/worker.clj
+++ partition/src/partition/worker.clj
@@ -1,7 +1,14 @@
 (ns partition.worker
   (:require [delaunay-triangulation.core :as delaunay]))
 
+(defn delaunay-triangulation
+  [points]
+  (let [{:keys [points edges triangles]} (delaunay/triangulate points)]
+    (println "point" points)
+    (println "edge" edges)
+    (println "triangle" triangles)))
+
 (defn run
   "Run."
   []
-  (println "Hey."))
+  (delaunay-triangulation [[2 3] [-4 9.3] [4 3] [2 2]]))
