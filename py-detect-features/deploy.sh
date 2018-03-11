7098d3d21d1f88c0ffdc727368d7b3a8196bbc89
Ben Wisialowski <bwisialowski@gmail.com>
2018-03-10 22:46:28 -0600

    run locally!

--- py-detect-features/deploy.sh
+++ py-detect-features/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+cp -r linux-libs/cv2 .
+cp -r linux-libs/numpy .
 zip -r9 py-detect-features.zip * -x *.zip
+rm -r cv2
+rm -r numpy
 aws lambda update-function-code --function-name lowpoly-py-detect-features --zip-file fileb://$(pwd)/py-detect-features.zip
--- py-detect-features/lambda_function.py
+++ py-detect-features/lambda_function.py
@@ -1,8 +1,11 @@
 import cv2
 
+
 def lambda_handler(event, context):
     print "OpenCV installed version:", cv2.__version__
     return "It works!"
 
 if __name__ == "__main__":
     lambda_handler(42, 42)
+
+# python -m py-detect-features.lambda_function
