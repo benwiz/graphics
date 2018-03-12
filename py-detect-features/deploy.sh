62cf924c22b578ec131176026e03567c6b5409f6
Ben Wisialowski <bwisialowski@gmail.com>
2018-03-11 19:17:17 -0500

    add stuff

--- py-detect-features/deploy.sh
+++ py-detect-features/deploy.sh
@@ -5,20 +5,8 @@
 # Remove old zip
 rm py-detect-features.zip
 
-# Move libs to root
-mv libs/cv2 .
-mv libs/numpy .
-mv libs/matplotlib .
-mv libs/Pillow-5.0.0.dist-info .
-
 # Create zip artifact
 zip -r9 py-detect-features.zip * -x *.zip -x deploy.sh
-
-# Put libs back
-mv cv2 libs/
-mv numpy libs/
-mv matplotlib libs/
-mv Pillow-5.0.0.dist-info libs/
 
 # Upload zip to S3
 aws s3 cp py-detect-features.zip s3://lowpoly/py-detect-features.zip --profile personal
