b27fe45a3e35e288f2f77a593cb8544053abf70e
Ben Wisialowski <bwisialowski@gmail.com>
2018-03-15 09:22:00 -0500

    order of events

--- py-detect-features/lambda_function.py
+++ py-detect-features/lambda_function.py
@@ -81,11 +81,6 @@ def lambda_handler(event, context):
                     point = (x, y)
                     points.append(point)
 
-    # Upload key points as JSON to S3
-    data = json.dumps(points)
-    points_key = session_id + '/points.json'
-    s3.Object(BUCKET_NAME, points_key).put(Body=data)
-
     # Draw on img
     for point in points:
         center = point
@@ -98,6 +93,12 @@ def lambda_handler(event, context):
     img = cv2.imencode('.jpg', img)[1].tostring()
     img_key = session_id + '/points.jpg'
     s3.Object(BUCKET_NAME, img_key).put(Body=img)
+
+    # Upload key points as JSON to S3. Upload this second because this is the
+    # trigger for the partition phase.
+    data = json.dumps(points)
+    points_key = session_id + '/points.json'
+    s3.Object(BUCKET_NAME, points_key).put(Body=data)
 
 if __name__ == "__main__":
     event = {u'Records': [{u'eventVersion': u'2.0', u'eventTime': u'2018-03-11T14:50:46.631Z', u'requestParameters': {u'sourceIPAddress': u'98.163.206.197'}, u's3': {u'configurationId': u'367c003d-db1a-4a71-9e34-b47f90c71a86', u'object': {u'eTag': u'fa02ebd6d522c72806a428c309d13756', u'sequencer': u'005AA54246862A53B6', u'key': u'748205b7-c744-4be2-ae9a-5f49f34abf73/start.jpg', u'size': 162446}, u'bucket': {u'arn': u'arn:aws:s3:::lowpoly', u'name': u'lowpoly', u'ownerIdentity': {u'principalId': u'AX2FA51TPHMAJ'}}, u's3SchemaVersion': u'1.0'}, u'responseElements': {u'x-amz-id-2': u'xhK79IlgCRf1wX7Xh8imG7+xSbtZfl9AQJIPVkzUazYyetsFVKI2MSz4aC7q3moZSzZyvE4WYNM=', u'x-amz-request-id': u'F4A63ED2826C8B0D'}, u'awsRegion': u'us-east-1', u'eventName': u'ObjectCreated:Put', u'userIdentity': {u'principalId': u'AX2FA51TPHMAJ'}, u'eventSource': u'aws:s3'}]}
