1e7cdd2ac9a023752d08ba4c03e5fe469215d056
Ben Wisialowski <bwisialowski@gmail.com>
2018-03-18 14:41:34 -0500

    try to figure out how to sum

--- color/src/color/worker.clj
+++ color/src/color/worker.clj
@@ -79,41 +79,39 @@
                       y-range (range
                                (apply min (map (fn [point] (get point 1)) triangle))
                                (apply max (map (fn [point] (get point 1)) triangle)))
-                      points (map (fn [x]
-                                    [x
-                                     (filter identity (map (fn [y]
-                                                             (if
-                                                              (= (Math/abs
-                                                                  (+
-                                                                   (* x1 (- y2 y3))
-                                                                   (* x2 (- y3 y1))
-                                                                   (* x3 (- y1 y2))))
-                                                                 (+ (Math/abs
-                                                                     (+
-                                                                      (* x1 (- y2 y))
-                                                                      (* x2 (- y y1))
-                                                                      (* x (- y1 y2))))
-                                                                    (Math/abs
-                                                                     (+
-                                                                      (* x1 (- y y3))
-                                                                      (* x (- y3 y1))
-                                                                      (* x3 (- y1 y))))
-                                                                    (Math/abs
-                                                                     (+
-                                                                      (* x (- y2 y3))
-                                                                      (* x2 (- y3 y))
-                                                                      (* x3 (- y y2))))))
-                                                               [(.getRed (Color. (.getRGB bi x y)))
+                      colors (map (fn [x]
+                                    (filter identity (map (fn [y]
+                                                            (if
+                                                             (= (Math/abs
+                                                                 (+
+                                                                  (* x1 (- y2 y3))
+                                                                  (* x2 (- y3 y1))
+                                                                  (* x3 (- y1 y2))))
+                                                                (+ (Math/abs
+                                                                    (+
+                                                                     (* x1 (- y2 y))
+                                                                     (* x2 (- y y1))
+                                                                     (* x (- y1 y2))))
+                                                                   (Math/abs
+                                                                    (+
+                                                                     (* x1 (- y y3))
+                                                                     (* x (- y3 y1))
+                                                                     (* x3 (- y1 y))))
+                                                                   (Math/abs
+                                                                    (+
+                                                                     (* x (- y2 y3))
+                                                                     (* x2 (- y3 y))
+                                                                     (* x3 (- y y2))))))
+                                                              [(.getRed (Color. (.getRGB bi x y)))
                                                                (.getGreen (Color. (.getRGB bi x y)))
                                                                (.getBlue (Color. (.getRGB bi x y)))
                                                                (.getAlpha (Color. (.getRGB bi x y)))]
-                                                               nil))
-                                                           y-range))])
-                                  x-range)
-                      r 0
-                      g 0
-                      b 0]
-                  (println "points:" points))
+                                                              nil))
+                                                          y-range)))
+                                  x-range)]
+                  (println "colors:" (type colors))
+                  ; (println "sum:" (apply map + (take 1 (take 1 colors))))
+                  (println "Done."))
 
                 ; Fill polygon with a color
                 (.setColor g (Color. (rand-int 256) (rand-int 256) (rand-int 256)))
