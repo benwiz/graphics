b4ba27c20d5da33eba4593f96b630c710150e9ce
Ben Wisialowski <bwisialowski@gmail.com>
2018-05-06 19:01:04 -0500

    revert

--- src/squiggle/core.cljs
+++ src/squiggle/core.cljs
@@ -1,9 +1,10 @@
-(ns squiggle.core)
+(ns squiggle.core
+    (:require [quil.core :as q]
+              [quil.middleware :as m]))
 
 (println "Hello!")
 (.setProperty
  (.-style
   (.querySelector js/document "#myspan"))
- "color"
- "red")
+ "color" "red")
 (.log js/console "Hi, from ClojureScript!")
