12450ba9dd8841760c773f2ee3231d36e3d00557
Ben Wiz <bwisialowski@gmail.com>
2018-10-10 20:58:11 +0800

    set up structure

--- src/snakes.cpp
+++ src/snakes.cpp
@@ -10,7 +10,18 @@
 Snakes::Snakes() {}
 
 void Snakes::setup(int n) {
-  
+  int width = 0.8 * ofGetWidth();
+  int height = 0.8 * ofGetHeight();
+  // Create each snake
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      int x = i * width;
+      int y = j * height;
+      Snake snake;
+      snake.setup();
+      snakes.push_back(snake);
+    }
+  }
 }
 
 void Snakes::update() {
@@ -26,6 +37,10 @@ void Snakes::draw() {
   background.rectangle(0, 0, 0.8 * ofGetWidth(), 0.8 * ofGetHeight());
   background.setFillColor(ofColor::mintCream);
   background.draw();
+
+  for (Snake &snake : snakes) {
+    snake.draw();
+  }
 
   ofPopMatrix();
 }
