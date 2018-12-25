4e1e406c7bfe47a552c8d0bb03a491eba4221238
Ben Wiz <bwisialowski@gmail.com>
2018-12-25 11:43:12 -0800

    use .mjs, not sure if that is smrt

--- README-developer.md
+++ README-developer.md
@@ -14,6 +14,5 @@ Deploy
 npm publish --access public
 ```
 
-
 ## To Do
 
---
+++ index.mjs
@@ -0,0 +1,5 @@
+export const printMsg = () => {
+  console.log('This is a message from the demo package. Pt. 1.');
+};
+
+export default null;
