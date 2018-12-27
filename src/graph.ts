b65d7197f1c6d3d86dbe43c0cdc93dee66cc3cbd
Ben Wiz <bwisialowski@gmail.com>
2018-12-27 09:25:40 -0800

    dfs prep work

--- src/graph.ts
+++ src/graph.ts
@@ -1,15 +1,22 @@
 const dfsUtil = (
   adjList: Adjacency[],
+  startPointID: number,
   pointID: number,
   visited: Boolean[],
+  depth: number,
 ): void => {
+  // If we have reached maxDepth-1 (because we count from 0), then check if the starting point
+  // is a neighbor of the current point. If it is, we must save that triangle.
+
   visited[pointID] = true;
-  console.log(pointID);
+  console.log(pointID, depth);
+
+  depth += 1;
 
   const neighborIDs = adjList[pointID].neighborIDs;
   for (const neighborID of neighborIDs) {
     if (!visited[neighborID]) {
-      dfsUtil(adjList, neighborID, visited);
+      dfsUtil(adjList, startPointID, neighborID, visited, depth);
     }
   }
 };
@@ -20,5 +27,7 @@ export const dfs = (adjList: Adjacency[]): void => {
     visited[i] = false;
   }
 
-  dfsUtil(adjList, adjList[0].pointID, visited);
+  const depth = 0;
+  const startPointID = adjList[0].pointID;
+  dfsUtil(adjList, startPointID, startPointID, visited, depth);
 };
