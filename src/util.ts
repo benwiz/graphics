5b2777c76e750dc59f2f3ff4e8f95526b9c40843
Ben Wiz <bwisialowski@gmail.com>
2018-12-26 22:43:26 -0800

    distance function

--- src/util.ts
+++ src/util.ts
@@ -7,3 +7,13 @@ export const getRandomInt = (min: number, max: number): number =>
 export const degToRadians = (angle: number) => angle * (Math.PI / 180);
 
 export const radiansToDeg = (angle: number) => angle * (180 / Math.PI);
+
+export const distance = (point1: Point, point2: Point) => {
+  // sqrt( (x1 - x2)^2 + (y1 - y2)^2 )
+  const x1 = point1.x;
+  const y1 = point1.y;
+  const x2 = point2.x;
+  const y2 = point2.y;
+  const dist = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+  return dist;
+};
