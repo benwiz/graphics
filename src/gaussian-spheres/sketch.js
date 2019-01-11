5f9b3dcba9787de2f7d36b1ad8480b56a865bd6d
Ben Wiz <bwisialowski@gmail.com>
2019-01-11 09:29:21 -0800

    gaussian distribution of cubes

--- src/gaussian-spheres/sketch.js
+++ src/gaussian-spheres/sketch.js
@@ -6,18 +6,40 @@ global.THREE = THREE;
 // Include any additional ThreeJS examples below
 require('three/examples/js/controls/OrbitControls');
 
+const randomBM = (min, max, skew) => {
+  let u = 0;
+  let v = 0;
+  while (u === 0) u = Math.random(); // Converting [0,1) to (0,1)
+  while (v === 0) v = Math.random();
+  let num = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+
+  num = num / 10.0 + 0.5; // Translate to 0 -> 1
+  if (num > 1 || num < 0) num = randomBM(min, max, skew); // resample between 0 and 1 if out of range
+  num **= skew; // Skew
+  num *= max - min; // Stretch to fill range
+  num += min; // offset to min
+  return num;
+};
+
 const createSpheres = (n) => {
   const meshes = [];
 
+  const colors = ['red', 'blue', 'yellow'];
+
   for (let i = 0; i < n; i += 1) {
-    const mesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshPhysicalMaterial({
-        color: 'white',
-        roughness: 0.75,
-        flatShading: true,
-      }),
-    );
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshPhysicalMaterial({
+      color: colors[i % colors.length],
+      roughness: 0.75,
+      flatShading: true,
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+
+    const x = randomBM(-10, 10, 1);
+    const y = randomBM(-10, 10, 1);
+    const z = randomBM(-10, 10, 1);
+    mesh.position.set(x, y, z);
+
     meshes.push(mesh);
   }
 
@@ -45,7 +67,7 @@ export const setup = (options) => {
   const scene = new THREE.Scene();
 
   // Create the mesh and add to the scene
-  const meshes = createSpheres(3);
+  const meshes = createSpheres(30);
   meshes.forEach(mesh => scene.add(mesh));
 
   // Specify an ambient/unlit colour
