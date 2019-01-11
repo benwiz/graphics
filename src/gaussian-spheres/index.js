793ac0bc1b850f4d589df2062c48f45babfd0500
Ben Wiz <bwisialowski@gmail.com>
2019-01-11 08:40:58 -0800

    start gaussian spheres

---
+++ src/gaussian-spheres/index.js
@@ -0,0 +1,23 @@
+import CanvasSketch from 'canvas-sketch';
+import P5 from 'p5';
+import * as Sketch from './sketch';
+
+const preload = (p5) => {
+  // You can use p5.loadImage() here, etc...
+};
+
+const settings = {
+  // Pass the p5 instance, and preload function if necessary
+  p5: { p5: P5, preload },
+  // Turn on a render loop
+  animate: false,
+  // Configure size
+  dimensions: 'letter',
+};
+
+const sketch = (canvasSketch) => {
+  Sketch.setup(canvasSketch.p5);
+  return Sketch.draw;
+};
+
+CanvasSketch(sketch, settings);
