628beb8b7b13986fc03183b6e14c3e28559bd378
Ben Wiz <bwisialowski@gmail.com>
2019-02-13 12:44:47 -0500

    fill golden ratio rect black

--- src/wet-and-sharp/sketch.js
+++ src/wet-and-sharp/sketch.js
@@ -1,15 +1,23 @@
+const GOLDEN_RATIO = 1 / 1.61803398875;
+
 //
 // All setup functions here
 //
 
-export const setup = (options) => {
-  
-};
+export const setup = (options) => {};
 
 //
 // All drawing functions below here
 //
 
-export const draw = (options) => {
+const drawing1 = (p5) => {
+  // Draw the bottom (golden) half of the canvas black
+  p5.fill(0);
+  p5.rect(0, p5.height * GOLDEN_RATIO, p5.width, p5.height * (1 - GOLDEN_RATIO));
+};
 
+export const draw = (options) => {
+  const { p5 } = options;
+
+  drawing1(p5);
 };
