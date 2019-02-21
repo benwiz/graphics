0362f845953f2aad18d626f8f795497416feff39
Ben Wiz <bwisialowski@gmail.com>
2019-02-20 20:51:29 -0500

    draw a copy not quite mirror

--- src/noisy-shapes/sketch.js
+++ src/noisy-shapes/sketch.js
@@ -67,7 +67,7 @@ const noisyShapes = (p5) => {
   p5.rect(p5.width / 2, 0, p5.width / 2, p5.height);
 };
 
-const roughShapes = (p5) => {
+const roughShapesRandom = (p5) => {
   const drawSquare = (canvas, side) => {
     // Set random location and size
     const minX = 0.1 * p5.width;
@@ -127,9 +127,73 @@ const roughShapes = (p5) => {
   }
 };
 
+const roughShapesMirror = (p5) => {
+  const createRect = (canvas) => {
+    // Set random location and size
+    const minX = 0.1 * p5.width;
+    const maxX = (0.8 * p5.width) / 2;
+    const minY = 0.1 * p5.height;
+    const maxY = 0.8 * p5.height;
+    const minW = 0.02 * p5.width;
+    const maxW = 0.1 * p5.width;
+
+    const x = Util.getRandomFloat(minX, maxX);
+    const y = Util.getRandomFloat(minY, maxY);
+    const w = Util.getRandomFloat(minW, maxW);
+    const h = w;
+
+    const rect = {
+      x,
+      y,
+      w,
+      h,
+    };
+    const options = {
+      roughness: p5.randomGaussian(2.5),
+      stroke: 'black',
+      fill: 'white',
+    };
+
+    return { rect, options };
+  };
+
+  // Set up rough.js canvas
+  const canvas = Rough.canvas(document.querySelector('canvas'));
+
+  // Set background color to be white
+  p5.background(255);
+
+  // Make the right half black
+  canvas.rectangle(p5.width / 2, 0, p5.width / 2, p5.height, {
+    roughness: 0,
+    strokeWidth: 0,
+    fill: 'black',
+    fillStyle: 'solid', // 'hachure',
+    fillWeight: 3,
+    hachureGap: 3,
+  });
+
+  // Create and draw a few rects
+  const numRects = Util.getRandomInt(3, 5);
+  for (let i = 0; i < numRects; i++) {
+    const { rect, options } = createRect(canvas);
+
+    // Draw the left side
+    canvas.rectangle(rect.x, rect.y, rect.w, rect.h, options);
+
+    // Draw the right side
+    rect.x += p5.width / 2;
+    options.roughness *= 2;
+    options.stroke = 'white';
+    options.fill = 'black';
+    canvas.rectangle(rect.x, rect.y, rect.w, rect.h, options);
+  }
+};
+
 export const draw = (options) => {
   const { p5 } = options;
 
   // noisyShapes(p5);
-  roughShapes(p5);
+  // roughShapesRandom(p5);
+  roughShapesMirror(p5);
 };
