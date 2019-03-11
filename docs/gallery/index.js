2b579b5a03a891fc07477e98db5d95095ca6f73e
Ben Wiz <bwisialowski@gmail.com>
2019-03-11 15:50:16 -0500

    dont pad

--- docs/gallery/index.js
+++ docs/gallery/index.js
@@ -12,16 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const imagesDiv = document.querySelector('#images');
   for (let i = 0; i < filenames.length; i++) {
     const filename = filenames[i];
-    const imgHTML = `<img src="./images/${filename}" width="360"></img>`;
+    const imgHTML = `<img src="./images/${filename}" width="360" height="360"></img>`;
     imagesDiv.innerHTML += imgHTML;
   }
-
-  // TODO: Fix this
-  // // Pad with empty images
-  // const numPads = filenames.length % 3;
-  // const imgHTML = '<img src="" width="360"></img>';
-  // console.log(numPads);
-  // for (let i = 0; i < numPads; i++) {
-  //   imagesDiv.innerHTML += imgHTML;
-  // }
 });
