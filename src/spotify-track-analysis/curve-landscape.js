dc281f395bb46cbdbf37caf2cff1d9684527db1d
Ben Wiz <bwisialowski@gmail.com>
2019-05-15 14:20:25 -0500

    bad shifts of y value

--- src/spotify-track-analysis/curve-landscape.js
+++ src/spotify-track-analysis/curve-landscape.js
@@ -22,10 +22,25 @@ const drawCurveLandscape = (p5, width, height, TrackAnalysis) => {
     p5.curveVertex(firstVertex.x, firstVertex.y);
     for (let i = 0; i < segment.timbre.length; i++) {
       const timbre = segment.timbre[i];
+      const previousVertex = {
+        x: ((i - 1) / (segment.timbre.length - 1)) * width,
+        y: y + 0.2 * segment.timbre[i - 1],
+      };
       const vertex = {
         x: (i / (segment.timbre.length - 1)) * width,
         y: y + 0.2 * timbre,
       };
+
+      // If vertex.y is above the previous vertex's y value, shift it down below
+      if (vertex.y <= previousVertex.y) {
+        vertex.y = previousVertex.y + 5;
+      }
+
+      // If vertex.y is below the previous vertex's y value, shift it up above
+      if (vertex.y >= previousVertex.y) {
+        vertex.x = previousVertex.y - 5;
+      }
+
       p5.curveVertex(vertex.x, vertex.y);
     }
     p5.curveVertex(lastVertex.x, lastVertex.y);
