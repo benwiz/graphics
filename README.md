23e4d2f59805aada0a232682cb0e9f5b4b453031
benwiz <bwisialowski@gmail.com>
2020-12-01 12:36:02 -0600

    good suff

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # graphics
 
 Monorepo of various graphics generating projects.
+
+## To Do
+
+- Save the current state of noise-scape-lines as oscilloscope
+- Look into using an image as noise
--- squiggle/src/com/benwiz/noise_scape.cljs
+++ squiggle/src/com/benwiz/noise_scape.cljs
@@ -7,7 +7,7 @@
   ;; (q/background 255)
   (q/stroke 0 0 0)
   {:t 0.02
-   :y (* 0 (q/height))
+   :y (* 0.33 (q/height))
    :max-y (* 0.9 (q/height))})
 
 (defn update-state
@@ -16,27 +16,30 @@
         t-delta (* 0.0066 y-delta)]
     (-> state
         (update :t + t-delta)
-        (update :y + y-delta))))
+        #_(update :y + y-delta))))
 
 (defn draw
   [{:keys [t y max-y]}]
+  (q/background 255)
   (if (> y max-y)
     (q/no-loop)
     (when-let [frame @listen/current-frame]
       (doseq [x (range 1 (q/width))]
         (q/line (dec x) (+ y
                            ;; Perlin Noise
-                           (* (* (q/height) 0.1)
+                           #_(* (* (q/height) 0.1)
                                 (q/noise (/ (dec x) 100) t))
                            ;; Microphone Noise
-                           #_(q/map-range (js/Math.abs (aget frame (int (q/map-range (dec x) 0 (dec (q/width)) 0 4096))))
-                                        0 15000 0 (/ (q/height) 2)))
+                           (q/map-range (aget frame (int (q/map-range (dec x) 0 (dec (q/width)) 0 4096)))
+                                        -20000 20000
+                                        (- (* 0.1 (q/height))) (* 0.1 (q/height))))
                 x (+ y
                      ;; Perlin Noise
-                     (* (* (q/height) 0.1)
+                     #_(* (* (q/height) 0.1)
                         (q/noise (/ x 100) t))
-                     #_(q/map-range (js/Math.abs (aget frame (int (q/map-range x 0 (dec (q/width)) 0 4096))))
-                                  0 15000 0 (/ (q/height) 2))))))))
+                     (q/map-range (aget frame (int (q/map-range x 0 (dec (q/width)) 0 4096)))
+                                  -20000 20000
+                                  (- (* 0.1 (q/height))) (* 0.1 (q/height)))))))))
 
 (defn key-pressed
   []
